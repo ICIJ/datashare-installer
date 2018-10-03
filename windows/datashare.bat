45df70b582e2dabb460b34b8c5a385f0fda3d6e4
Bruno Thomas <bruno@barreverte.fr>
2018-10-03 08:35:32 +0000

    [windows] launcher waits for datashare to start

--- windows/datashare.bat
+++ windows/datashare.bat
@@ -1,4 +1,24 @@
 @echo off
 docker-compose up -d
-start "" http://localhost:8080
-pause
+
+call :wait_idx_is_up
+
+if not ERRORLEVEL 1 (
+  start "" http://localhost:8080
+)
+
+exit /B %ERRORLEVEL%
+
+:wait_idx_is_up
+echo|set /p="waiting for datashare to be up"
+for /l %%x in (1, 1, 30) do (
+    ping -n 1 127.0.0.1 >nul
+    curl --silent localhost:8080 >nul
+    if not ERRORLEVEL 1 (
+        echo OK
+        exit /B 0
+    )
+    echo|set /p="."
+)
+echo KO
+exit /B %ERRORLEVEL%
