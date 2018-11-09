dca31b0e54310e2e76ee4ecea6e2cb0798fc4bed
Bruno Thomas <bruno@barreverte.fr>
2018-11-09 14:55:05 +0000

    [deploy] adds a deploy script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+repo=ICIJ/datashare-installer
+release=$1
+
+if [[ -z "$release" ]]; then
+  echo "usage: $0 <release_name>"
+  exit 1
+fi
+
+upload_url=$(curl -s -H "Authorization: token $GITHUB_TOKEN" -d "{\"tag_name\":\"${release}\", \"name\":\"${release}\",\"body\":\"release ${release}\"}" "https://api.github.com/repos/$repo/releases" | jq -r '.upload_url')
+upload_url="${upload_url%\{*}"
+
+echo "uploading asset to release to url : $upload_url"
+
+curl -s -H "Authorization: token $GITHUB_TOKEN" -H "Content-Type: application/x-xar" --data-binary @mac/dist/Datashare.pkg "$upload_url?name=Datashare.pkg&label=Datashare.pkg"
+curl -s -H "Authorization: token $GITHUB_TOKEN" -H "Content-Type: application/vnd.microsoft.portable-executable" --data-binary @windows/dist/installDatashare.exe "$upload_url?name=installDatashare.exe&label=installDatashare.exe"
