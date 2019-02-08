7691cbc56733e5d4da6e16d3a513d64047ab5239
Bruno Thomas <bruno@barreverte.fr>
2019-02-08 09:40:59 -0800

    [windows] datashare powershell script
    see https://github.com/ICIJ/datashare/issues/76

--- windows/datashare.ps1
+++ windows/datashare.ps1
@@ -1,30 +1,45 @@
-$ret = iex "docker info"
-if ($?) { $ret }
-else {
-    "docker is not running, launching it"
-    iex "& C:\Program Files\Docker\Docker\Docker for Windows.exe"
+function WaitDockerIsUp {
+    Write-Host "Waiting for docker to be up" -NoNewLine
+    foreach ($nb in 1..60) {
+        docker info 2>($tmpFile=New-TemporaryFile)
+        if ($LASTEXITCODE -eq 0) { return 0 }
+        Write-Host "." -NoNewLine
+        Start-Sleep 1
+    }
 }
 
 function WaitDatashareIsUp {
+    Write-Host "Waiting for datashare to be up." -NoNewLine
     foreach ($nb in 1..60) {
-        $ret = Test-NetConnection -ComputerName "localhost" -Port 8080
-        $ret
+        Start-Sleep 1
+        $Result = (Test-NetConnection "localhost" -Port 8080).TcpTestSucceeded
+        if ($Result) {
+            Write-Host "OK"
+            return
+        }
+        Write-Host "." -NoNewLine
     }
+}
+
+docker info 2>($tmpFile=New-TemporaryFile)
+if ($LASTEXITCODE -ne 0) {
+    Start-Process 'C:\Program Files\Docker\Docker\Docker for Windows.exe'
+    & WaitDockerIsUp
 }
 
 $datashare_id = iex "docker-compose -p datashare ps -q datashare"
 $datashare_status=""
 if ($datashare_id) {
-    docker inspect $datashare_id -f "{{.State.Status}}"
+    $datashare_status = iex 'docker inspect $datashare_id -f "{{.State.Status}}"'
 }
-$mem_size = (systeminfo | Select-String 'Total Physical Memory:').ToString().Split(':')[1].Trim()
-$mem_allocated = $mem_size/(2*1024)
-$env:DS_JAVA_OPTS = "-Xmx$mem_allocated"
+$mem_size = (Get-WmiObject -Class Win32_ComputerSystem | out-string -stream |Select-String 'TotalPhysicalMemory').toString().split(':')[1].trim()
+$mem_allocated = [Int]($mem_size/(2*1024*1024))
+$env:DS_JAVA_OPTS = -join("-Xmx", $mem_allocated, "m")
 
 if ($datashare_status -eq "running") {
-    iex "docker-compose -p datashare restart datashare"
+    docker-compose -p datashare restart datashare
 } else {
-    iex "docker-compose -p datashare up -d"
+    docker-compose -p datashare up -d
 }
 
 & WaitDatashareIsUp
