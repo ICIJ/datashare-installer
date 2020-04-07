39960a55915233b28abff54d32c5e074aa991146
Maxime Vanza <maximevanza@live.fr>
2020-04-07 10:30:13 +0000

    [ICIJ/datashare#360] fix missing char in batch windows installer

--- windows/datashareStandalone.bat
+++ windows/datashareStandalone.bat
@@ -2,4 +2,4 @@
 
 cd %APPDATA%\Datashare # needed for /dist
 
-java -cp "dist;\Program Files\Datashare-${VERSION}\Datashare-${VERSION}-all.jar" -DPROD_MODE=true org.icij.datashare.Main -d %APPDATA\Datashare\data --queueType memory --busType memory --dataSourceUrl jdbc:sqlite:file:%APPDATA%\Datashare\dist\datashare.db --configFile %APPDATA%\Datashare\dist\datashare.conf --mode EMBEDDED --browserOpenLink true --elasticsearchDataPath %APPDATA%\Datashare\index
+java -cp "dist;\Program Files\Datashare-${VERSION}\Datashare-${VERSION}-all.jar" -DPROD_MODE=true org.icij.datashare.Main -d %APPDATA%\Datashare\data --queueType memory --busType memory --dataSourceUrl jdbc:sqlite:file:%APPDATA%\Datashare\dist\datashare.db --configFile %APPDATA%\Datashare\dist\datashare.conf --mode EMBEDDED --browserOpenLink true --elasticsearchDataPath %APPDATA%\Datashare\index
