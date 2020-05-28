77e31dbee9d610e363ccbdf9b0828d95c73ceff2
Maxime Vanza <maximevanza@live.fr>
2020-05-28 15:32:50 +0000

    [fix] user path in batch file

--- windows/datashareStandalone.bat
+++ windows/datashareStandalone.bat
@@ -1,5 +1,5 @@
 @echo off
 
-cd %APPDATA%\Datashare
+cd "%APPDATA%"\Datashare
 
-java -cp "dist;\Program Files\Datashare-${VERSION}\Datashare-${VERSION}-all.jar" -DPROD_MODE=true -Djava.system.class.loader=org.icij.datashare.DynamicClassLoader org.icij.datashare.Main -d %APPDATA%\Datashare\data --queueType memory --busType memory --dataSourceUrl jdbc:sqlite:file:%APPDATA%\Datashare\dist\datashare.db --settings %APPDATA%\Datashare\dist\datashare.conf --mode EMBEDDED --browserOpenLink true --elasticsearchDataPath %APPDATA%\Datashare\index
+java -cp "dist;\Program Files\Datashare-${VERSION}\Datashare-${VERSION}-all.jar" -DPROD_MODE=true -Djava.system.class.loader=org.icij.datashare.DynamicClassLoader org.icij.datashare.Main -d "%APPDATA%"\Datashare\data --queueType memory --busType memory --dataSourceUrl jdbc:sqlite:file:"%APPDATA%"\Datashare\dist\datashare.db --settings "%APPDATA%"\Datashare\dist\datashare.conf --mode EMBEDDED --browserOpenLink true --elasticsearchDataPath "%APPDATA%"\Datashare\index
