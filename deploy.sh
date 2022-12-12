793391174e87f9f6a1fa335c4e184d22501de136
Pierre Romera <hello@pirhoo.com>
2022-12-12 17:15:23 +0100

    build: use the correct assets in the deploy script

--- deploy.sh
+++ deploy.sh
@@ -12,8 +12,17 @@ upload_url="${upload_url%\{*}"
 
 echo "uploading asset to release to url : $upload_url"
 
-curl -s -H "Authorization: token $GITHUB_TOKEN" -H "Content-Type: application/x-xar" --data-binary @mac/dist/datashare.pkg "$upload_url?name=Datashare.pkg&label=Datashare.pkg"
-curl -s -H "Authorization: token $GITHUB_TOKEN" -H "Content-Type: application/x-xar" --data-binary @mac/dist/datashareStandalone.pkg "$upload_url?name=DatashareStandalone.pkg&label=DatashareStandalone.pkg"
-curl -s -H "Authorization: token $GITHUB_TOKEN" -H "Content-Type: application/vnd.microsoft.portable-executable" --data-binary @windows/dist/installDatashare.exe "$upload_url?name=installDatashare.exe&label=installDatashare.exe"
-curl -s -H "Authorization: token $GITHUB_TOKEN" -H "Content-Type: application/vnd.microsoft.portable-executable" --data-binary @windows/dist/installDatashareStandalone.exe "$upload_url?name=installDatashareStandalone.exe&label=installDatashareStandalone.exe"
-curl -s -H "Authorization: token $GITHUB_TOKEN" -H "Content-Type: application/x-sh" --data-binary @linux/dist/datashare.sh "$upload_url?name=datashare.sh&label=datashare.sh"
+curl -s \
+  -H "Authorization: token $GITHUB_TOKEN" \
+  -H "Content-Type: application/x-xar" \
+  --data-binary "@mac/dist/datashare-$release.pkg" "$upload_url?name=Datashare.pkg&label=Datashare.pkg"
+
+curl -s \
+  -H "Authorization: token $GITHUB_TOKEN" \
+  -H "Content-Type: application/vnd.microsoft.portable-executable" \
+  --data-binary "@windows/dist/datashare-$release.exe" "$upload_url?name=installDatashare.exe&label=installDatashare.exe"
+  
+curl -s \
+  -H "Authorization: token $GITHUB_TOKEN" \
+  -H "Content-Type: application/x-sh" \
+  --data-binary "@linux/dist/datashare-$release.sh" "$upload_url?name=datashare.sh&label=datashare.sh"
