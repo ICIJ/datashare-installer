0332c90e885f51a2685f7982c6912b1946957871
Pierre Romera <hello@pirhoo.com>
2022-12-12 17:50:37 +0100

    feat: use new name params

--- deploy.sh
+++ deploy.sh
@@ -15,14 +15,14 @@ echo "uploading asset to release to url : $upload_url"
 curl -s \
   -H "Authorization: token $GITHUB_TOKEN" \
   -H "Content-Type: application/x-xar" \
-  --data-binary "@mac/dist/datashare-$release.pkg" "$upload_url?name=Datashare.pkg&label=Datashare.pkg"
+  --data-binary "@mac/dist/datashare-$release.pkg" "$upload_url?name=datashare-$release.pkg&label=datashare-$release.pkg"
 
 curl -s \
   -H "Authorization: token $GITHUB_TOKEN" \
   -H "Content-Type: application/vnd.microsoft.portable-executable" \
-  --data-binary "@windows/dist/datashare-$release.exe" "$upload_url?name=installDatashare.exe&label=installDatashare.exe"
+  --data-binary "@windows/dist/datashare-$release.exe" "$upload_url?name=datashare-$release.exe&label=datashare-$release.exe"
   
 curl -s \
   -H "Authorization: token $GITHUB_TOKEN" \
   -H "Content-Type: application/x-sh" \
-  --data-binary "@linux/dist/datashare-$release.sh" "$upload_url?name=datashare.sh&label=datashare.sh"
+  --data-binary "@linux/dist/datashare-$release.sh" "$upload_url?name=datashare-$release.sh&label=datashare-$release.sh"
