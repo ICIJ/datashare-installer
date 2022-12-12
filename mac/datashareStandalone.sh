54beda2aed39695c672bc34fc1495cc030c85919
Pierre Romera <hello@pirhoo.com>
2022-12-12 10:49:15 +0100

    feat: ensure Datashare starts once on Mac

--- mac/datashareStandalone.sh
+++ mac/datashareStandalone.sh
@@ -1,26 +1,53 @@
 #!/bin/bash
 
-DATASHARE_VERSION=__version__
+# Get the path to the PID file
+pid_file="/tmp/datashare.pid"
+pid=$(cat $pid_file 2>/dev/null)
+# Collect Datashare conviguration env
+datashare_version=__version__
+datashare_data_path="/Users/${USER}/Datashare"
+datashare_index_path="/Users/${USER}/Library/Datashare/index"
+datashare_plugins_path="/Users/${USER}/Library/datashare/plugins"
+datashare_extensions_path="/Users/${USER}/Library/datashare/extensions"
+# Java binary to use to start Datashare
+java_bin=/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jre/Contents/Home/bin/java
 
-DATA_PATH="/Users/${USER}/Datashare"
-INDEX_PATH="/Users/${USER}/Library/Datashare/index"
-PLUGINS_PATH="/Users/${USER}/Library/datashare/plugins"
-EXTENSIONS_PATH="/Users/${USER}/Library/datashare/extensions"
-JAVA=/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jre/Contents/Home/bin/java
+function on_exit_remove_pid_file {
+    rm -f $pid_file
+}
 
-mkdir -p "${INDEX_PATH}" "${PLUGINS_PATH}" "${EXTENSIONS_PATH}"
+function save_pid {
+    echo $$ > "$pid_file"
+}
 
-if [[ -z "${DS_JAVA_OPTS}" ]]; then
-    mem_allocated=$(sysctl -a | grep hw.memsize | awk '{print $2"/(2*1024^2)"}' | bc)
-    DS_JAVA_OPTS="-Xmx${mem_allocated}m"
+function start_datashare {
+    mkdir -p "${datashare_index_path}" "${datashare_plugins_path}" "${datashare_extensions_path}"
+
+    if [[ -z "${DS_JAVA_OPTS}" ]]; then
+        mem_allocated=$(sysctl -a | grep hw.memsize | awk '{print $2"/(2*1024^2)"}' | bc)
+        DS_JAVA_OPTS="-Xmx${mem_allocated}m"
+    fi
+
+    cd "/Users/${USER}/Library/Datashare" || exit # needed for /dist
+
+    $java_bin -cp "./dist:/Applications/Datashare.app/Contents/Resources/datashare-dist-${datashare_version}-all.jar" \
+        -DPROD_MODE=true -Dfile.encoding=UTF-8 -Djava.system.class.loader=org.icij.datashare.DynamicClassLoader \
+        org.icij.datashare.Main -d "$datashare_data_path" --queueType memory --busType memory \
+        --dataSourceUrl jdbc:sqlite:file:"/Users/${USER}/Library/Datashare/dist/datashare.db" \
+        --settings ./dist/datashare.conf --mode EMBEDDED --browserOpenLink true \
+        --elasticsearchDataPath "$datashare_index_path" --pluginsDir "${datashare_plugins_path}" --extensionsDir "${datashare_extensions_path}"
+}
+
+# Ensure the PID file is removed when the script exits
+trap on_exit_remove_pid_file EXIT
+
+# Check if the PID file exists and is already running
+if [ -f "$pid_file" ] && kill -0 $pid > /dev/null 2>&1
+then
+    echo "Datashare is already running (PID: $pid)" 
+else
+    # If the PID file does not exist, store its PID in the PID file
+    save_pid
+    # Finally, start Datashare
+    start_datashare
 fi
-
-cd "/Users/${USER}/Library/Datashare" || exit # needed for /dist
-
-$JAVA -cp "./dist:/Applications/Datashare.app/Contents/Resources/datashare-dist-${DATASHARE_VERSION}-all.jar" \
-    -DPROD_MODE=true -Dfile.encoding=UTF-8 -Djava.system.class.loader=org.icij.datashare.DynamicClassLoader \
-    org.icij.datashare.Main -d "$DATA_PATH" --queueType memory --busType memory \
-    --dataSourceUrl jdbc:sqlite:file:"/Users/${USER}/Library/Datashare/dist/datashare.db" \
-    --settings ./dist/datashare.conf --mode EMBEDDED --browserOpenLink true \
-    --elasticsearchDataPath "$INDEX_PATH" --pluginsDir "${PLUGINS_PATH}" --extensionsDir "${EXTENSIONS_PATH}"
-
