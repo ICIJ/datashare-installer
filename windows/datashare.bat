38a2123766c862073d0722260498ad8ab9620b4a
Pierre Romera <hello@pirhoo.com>
2024-02-28 10:01:44 +0000

    fix: use APPDATA to find jar on windows

--- windows/datashare.bat
+++ windows/datashare.bat
@@ -2,7 +2,7 @@
 
 cd "%APPDATA%"\Datashare
 FOR /F "tokens=*" %%i IN ('where -f java ^| findstr -R "[jdk|jre]-" ^| findstr -R -v "[jdk|jre]-[0-9]\. [jdk|jre]-10" ^| cmd /e /v /q /c"set/p.=&&echo(^!.^!"') do SET java_exe=%%i
-%java_exe% -cp "dist;\Program Files\Datashare\datashare-dist-${VERSION}-all.jar" ^
+%java_exe% -cp "dist;%APPDATA%\Datashare\datashare-dist-${VERSION}-all.jar" ^
   --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED ^
   -DPROD_MODE=true -Dfile.encoding=UTF-8 ^
   -Djava.system.class.loader=org.icij.datashare.DynamicClassLoader org.icij.datashare.Main ^
