1930dd2df20c683f2f014a56836ee3bcefa118d2
Pierre Romera <hello@pirhoo.com>
2024-03-04 11:12:00 +0100

    fix: use relative path to Datashare home

--- windows/datashare.bat
+++ windows/datashare.bat
@@ -1,20 +1,28 @@
 @echo off
 
-cd "%APPDATA%"\Datashare
+:: Get the directory of the current file
+set "CURRENT_DIR=%~dp0"
+
+:: Remove the trailing backslash for consistency in path usage
+set "CURRENT_DIR=%CURRENT_DIR:~0,-1%"
+
+:: Ensure the current dir is active
+cd "%CURRENT_DIR%"
+
 FOR /F "tokens=*" %%i IN ('where -f java ^| findstr -R "[jdk|jre]-" ^| findstr -R -v "[jdk|jre]-[0-9]\. [jdk|jre]-10" ^| cmd /e /v /q /c"set/p.=&&echo(^!.^!"') do SET java_exe=%%i
-%java_exe% -cp "dist;%APPDATA%\Datashare\datashare-dist-${VERSION}-all.jar" ^
+%java_exe% -cp "dist;%CURRENT_DIR%\datashare-dist-${VERSION}-all.jar" ^
   --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED ^
   -DPROD_MODE=true -Dfile.encoding=UTF-8 ^
   -Djava.system.class.loader=org.icij.datashare.DynamicClassLoader org.icij.datashare.Main ^
-  --dataDir "%APPDATA%"\Datashare\data ^
+  --dataDir "%CURRENT_DIR%"\data ^
   --batchQueueType MEMORY ^
   --queueType MEMORY ^
   --busType MEMORY ^
-  --dataSourceUrl jdbc:sqlite:file:"%APPDATA%"\Datashare\dist\datashare.db ^
-  --settings "%APPDATA%"\Datashare\dist\datashare.conf ^
+  --dataSourceUrl jdbc:sqlite:file:"%CURRENT_DIR%"\dist\datashare.db ^
+  --settings "%CURRENT_DIR%"\dist\datashare.conf ^
   --mode EMBEDDED ^
   --browserOpenLink true ^
-  --elasticsearchDataPath "%APPDATA%"\Datashare\index ^
-  --pluginsDir "%APPDATA%"\Datashare\plugins ^
-  --extensionsDir "%APPDATA%"\Datashare\extensions
+  --elasticsearchDataPath "%CURRENT_DIR%"\index ^
+  --pluginsDir "%CURRENT_DIR%"\plugins ^
+  --extensionsDir "%CURRENT_DIR%"\extensions
 pause
