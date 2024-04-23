8fda457fc73ed57cd6fdb017b73f1125238c8059
Bruno Thomas <bthomas@icij.org>
2024-04-23 08:38:20 +0000

    fix: add java.net to opens ICIJ/datashare#1380

--- windows/datashare.bat
+++ windows/datashare.bat
@@ -11,7 +11,7 @@ cd "%CURRENT_DIR%"
 
 FOR /F "tokens=*" %%i IN ('where -f java ^| findstr -R "[jdk|jre]-" ^| findstr -R -v "[jdk|jre]-[0-9]\. [jdk|jre]-10" ^| cmd /e /v /q /c"set/p.=&&echo(^!.^!"') do SET java_exe=%%i
 %java_exe% -cp "dist;%CURRENT_DIR%\datashare-dist-${VERSION}-all.jar" ^
-  --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED ^
+  --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED --add-opens java.base/java.net=ALL-UNNAMED ^
   -DPROD_MODE=true -Dfile.encoding=UTF-8 ^
   -Djava.system.class.loader=org.icij.datashare.DynamicClassLoader org.icij.datashare.Main ^
   --dataDir "%CURRENT_DIR%"\data ^
