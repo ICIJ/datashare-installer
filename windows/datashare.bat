7dd834b2224715ea6da432a83ac4c774832121d9
Caroline Desprat <cdesprat@icij.org>
2025-06-30 07:19:27 +0000

    fix(windows): create the dist directory if it doesn't exist ICIJ/datashare#1747

--- windows/datashare.bat
+++ windows/datashare.bat
@@ -9,6 +9,11 @@ set "CURRENT_DIR=%CURRENT_DIR:~0,-1%"
 :: Ensure the current dir is active
 cd "%CURRENT_DIR%"
 
+:: Create the dist and default directories in current dir if they don't exist
+for %%d in (dist data index plugins extensions) do (
+    if not exist "%CURRENT_DIR%\%%d\" mkdir "%CURRENT_DIR%\%%d"
+)
+
 FOR /F "tokens=*" %%i IN ('where -f java ^| findstr -R "[jdk|jre]-" ^| findstr -R -v "[jdk|jre]-[0-9]\. [jdk|jre]-1[0-6]" ^| cmd /e /v /q /c"set/p.=&&echo(^!.^!"') do SET java_exe=%%i
 %java_exe% -cp "dist;%CURRENT_DIR%\datashare-dist-${VERSION}-all.jar" ^
   --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED --add-opens java.base/java.net=ALL-UNNAMED ^
